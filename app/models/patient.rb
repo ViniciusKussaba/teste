e5f52e70e1ba85da256b1d6c246812d5ea9f2ff4
Wallison de Oliveira <wallison@webgoal.com.br>
2022-10-31 16:35:33 -0300

    Adiciona a biblioteca annotate no projeto

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,2 +1,17 @@
+# == Schema Information
+#
+# Table name: patients
+#
+#  id                   :bigint           not null, primary key
+#  address_city         :string(255)
+#  address_neighborhood :string(255)
+#  address_state        :string(255)
+#  address_street       :string(255)
+#  birthdate            :date
+#  cpf                  :string(255)
+#  name                 :string(255)
+#  created_at           :datetime         not null
+#  updated_at           :datetime         not null
+#
 class Patient < ApplicationRecord
 end
