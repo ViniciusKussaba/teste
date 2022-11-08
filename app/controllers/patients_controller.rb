12642442062b5c70a3044bd9350798b7ac8295c0
W1 Consultoria <desenvolvimento@w1consultoria.com.br>
2022-11-08 10:20:35 -0300

    Adiciona a implementação da controller de pacientes

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,6 +1,9 @@
 class PatientsController < ApplicationController
+  before_action :set_patient, only: %i[ show edit update destroy ]
+
   # GET /patients or /patients.json
   def index
+    @patients = Patient.all
   end
 
   # GET /patients/1 or /patients/1.json
@@ -9,6 +12,7 @@ class PatientsController < ApplicationController
 
   # GET /patients/new
   def new
+    @patient = Patient.new
   end
 
   # GET /patients/1/edit
@@ -17,20 +21,49 @@ class PatientsController < ApplicationController
 
   # POST /patients or /patients.json
   def create
+    @patient = Patient.new(patient_params)
+
+    if @patient.save
+      redirect_to patient_url(@patient), notice: "Patient was successfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   # PATCH/PUT /patients/1 or /patients/1.json
   def update
+    if @patient.update(patient_params)
+      redirect_to patient_url(@patient), notice: "Patient was successfully updated."
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   # DELETE /patients/1 or /patients/1.json
   def destroy
+    @patient.destroy
+
+    redirect_to patients_url, notice: "Patient was successfully destroyed."
   end
 
   private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_patient
+      @patient = Patient.find(params[:id])
+    end
 
     # Only allow a list of trusted parameters through.
     def patient_params
-      params.require(:patient).permit(:name, :cpf, :birthdate, :address_street, :address_neighborhood, :address_city, :address_state)
+      params.require(:patient).permit(
+        %i[
+          name
+          cpf
+          birthdate
+          address_street
+          address_neighborhood
+          address_city
+          address_state
+        ]
+      )
     end
 end
